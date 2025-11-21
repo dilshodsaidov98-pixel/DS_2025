45815ce48e19cfcdee1b1ee519d6d48ff5041357
Dilshod Saidov <dilshodsaidov98@gmail.com>
2025-11-21 20:02:23 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title('🎈 IT')
 
 st.write('Hello world!')
+
+with st.expender ("DataFrame"):
+  st.write('**DATA**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
