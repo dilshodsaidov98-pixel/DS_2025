3d33b266b3e4c0a657453416b168a4dd45fd568d
Dilshod Saidov <dilshodsaidov98@gmail.com>
2025-11-24 17:44:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,23 @@
 import streamlit as st
-import pandas as pd 
-st.title('🎈 IT')
+import pandas as pd
 
-st.write('Hello world!')
+st.title('🤖 MY FIRST PROJECT')
 
-with st.expander ("DataFrame"):
-  st.write('**DATA**')
+st.write('Lets do it!')
+
+with st.expander("DATA"):
+  st.write('**OUR DATA**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+
+with st.expander("Visualization"):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+with st.sidebar:
+  st.header('Input features')
+  island = st.selectbox('**Island**', ('Torgersen', 'Dream', 'Biscoe'))
+  bill_length_mm = st.slider("**bill_length_mm**", 32.1, 59.6, 34.1)
+  bill_depth_mm = st.slider("**bill_depth_mm**", 32.1, 59.6, 45.1)
+  flipper_length_mm = st.slider("**flipper_length_mm**", 32.1, 59.6, 55.1)
+  body_mass_g = st.slider("**body_mass_g**", 2700, 6300, 4207)
+  sex = st.selectbox('**Gender**', ('male', 'female'))
